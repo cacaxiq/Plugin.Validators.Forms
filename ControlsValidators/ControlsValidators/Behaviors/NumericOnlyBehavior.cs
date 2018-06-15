f787f2f95893e14792c1631d9ec56b44ebd7a994
Carlos Henrique Rocha Gabriel <carloshenriquerg@gmail.com>
2018-06-15 19:47:52 -0300

    Set por padrão o teclado para numerico quando behavior "NumericOnlyBehavior" for utilizado

--- ControlsValidators/ControlsValidators/Behaviors/NumericOnlyBehavior.cs
+++ ControlsValidators/ControlsValidators/Behaviors/NumericOnlyBehavior.cs
@@ -5,6 +5,28 @@ namespace ControlsValidators.Behaviors
 {
     public class NumericOnlyBehavior : BehaviorBase
     {
+        protected override void OnAttachedTo(View bindable)
+        {
+            if (bindable is Entry entry)
+            {
+               entry.Keyboard = Keyboard.Numeric;
+            }
+
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(View bindable)
+        {
+            if (bindable is Entry entry)
+            {
+                entry.Keyboard = Keyboard.Default;
+            }
+
+            base.OnDetachingFrom(bindable);
+
+            Dispose(true);
+        }
+
         protected override void HandleTextChanged(object sender, TextChangedEventArgs e)
         {
             base.HandleTextChanged(sender, e);
